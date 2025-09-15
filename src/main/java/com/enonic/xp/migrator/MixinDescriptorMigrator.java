a6c8ad9aa2edaa096afa2e3ca06d8b9631a0983a
Anatol Sialitski <asi@enonic.com>
2025-09-15 14:02:29 +0200

    Migrate MixinDescriptor #5

---
+++ src/main/java/com/enonic/xp/migrator/MixinDescriptorMigrator.java
@@ -0,0 +1,33 @@
+package com.enonic.xp.migrator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.migrator.yml.MixinDescriptorYml;
+import com.enonic.xp.schema.mixin.Mixin;
+import com.enonic.xp.schema.mixin.MixinName;
+import com.enonic.xp.xml.parser.XmlMixinParser;
+
+public class MixinDescriptorMigrator
+    extends DescriptorMigrator
+{
+    @Override
+    public Object doMigrate( final ApplicationKey currentApplication, final Path source )
+        throws IOException
+    {
+        final Mixin.Builder builder = Mixin.create();
+
+        final XmlMixinParser parser = new XmlMixinParser();
+        parser.currentApplication( currentApplication );
+        parser.source( Files.readString( source ) );
+        parser.builder( builder );
+        parser.parse();
+
+        builder.name( MixinName.from( currentApplication, "_TEMP_" ) );
+
+        final Mixin descriptor = builder.build();
+        return new MixinDescriptorYml( descriptor );
+    }
+}
--- src/main/java/com/enonic/xp/migrator/yml/MixinDescriptorYml.java
+++ src/main/java/com/enonic/xp/migrator/yml/MixinDescriptorYml.java
@@ -3,11 +3,11 @@ package com.enonic.xp.migrator.yml;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import com.enonic.xp.form.Form;
-import com.enonic.xp.macro.MacroDescriptor;
 import com.enonic.xp.schema.LocalizedText;
+import com.enonic.xp.schema.mixin.Mixin;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class MacroDescriptorYml
+public class MixinDescriptorYml
 {
     public LocalizedText displayName;
 
@@ -15,7 +15,7 @@ public class MacroDescriptorYml
 
     public Form form;
 
-    public MacroDescriptorYml( final MacroDescriptor descriptor )
+    public MixinDescriptorYml( final Mixin descriptor )
     {
         displayName = LocalizeHelper.localizeProperty( descriptor.getDisplayName(), descriptor.getDisplayNameI18nKey() );
         description = LocalizeHelper.localizeProperty( descriptor.getDescription(), descriptor.getDescriptionI18nKey() );
