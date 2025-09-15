d2a8c6048e09884fe8791daf87a45ea646db4303
Anatol Sialitski <asi@enonic.com>
2025-09-15 15:19:34 +0200

    Migrate LayoutDescriptor #7

---
+++ src/main/java/com/enonic/xp/migrator/LayoutDescriptorMigrator.java
@@ -0,0 +1,33 @@
+package com.enonic.xp.migrator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.enonic.xp.app.ApplicationKey;
+import com.enonic.xp.descriptor.DescriptorKey;
+import com.enonic.xp.migrator.yml.LayoutDescriptorYml;
+import com.enonic.xp.region.LayoutDescriptor;
+import com.enonic.xp.xml.parser.XmlLayoutDescriptorParser;
+
+public class LayoutDescriptorMigrator
+    extends DescriptorMigrator
+{
+    @Override
+    public Object doMigrate( final ApplicationKey currentApplication, final Path source )
+        throws IOException
+    {
+        final LayoutDescriptor.Builder builder = LayoutDescriptor.create();
+
+        final XmlLayoutDescriptorParser parser = new XmlLayoutDescriptorParser();
+        parser.builder( builder );
+        parser.currentApplication( currentApplication );
+        parser.source( Files.readString( source ) );
+        parser.parse();
+
+        builder.key( DescriptorKey.from( currentApplication, "_TEMP_" ) );
+
+        final LayoutDescriptor descriptor = builder.build();
+        return new LayoutDescriptorYml( descriptor );
+    }
+}
